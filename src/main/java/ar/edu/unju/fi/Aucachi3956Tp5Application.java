a54c559f4775869875c3ad5b18f3fdc991dd36b8
Windows <Windows@DESKTOP-JD8CLH0>
2020-05-24 00:30:22 -0300

    Se agrego los proyectos

---
+++ src/main/java/ar/edu/unju/fi/Aucachi3956Tp5Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aucachi3956Tp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aucachi3956Tp5Application.class, args);
+	}
+
+}
